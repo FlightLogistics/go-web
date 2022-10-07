b46002cfadba5ff552b17d4f15df9251b02b9d3d
FlightLogistics <jb@flightlogistics.co.uk>
2022-10-07 13:16:47 +0100

    Initial Commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<H1>Good morning this is the goweb Service</H1>")
+		fmt.Fprintf(w, "<p>It runs on www.heliflyer.org.uk</p>")
+	})
+
+	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/greet/"):]
+		fmt.Fprintf(w, "Hello %s\n", name)
+	})
+
+	http.ListenAndServe(":9990", nil)
+}
